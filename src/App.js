c8ffbf0a6dde7f124f0615c655e06c774a657006
Marco <patrickstar1281@yahoo.com>
2022-11-03 06:26:42 -0500

    created a home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import About from './components/About';
 import Nav from './components/Nav';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
-        <About></About>
+        <Home></Home>
       </main>
     </div>
   );
