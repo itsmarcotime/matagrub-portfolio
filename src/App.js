16aa46fdabb81da5366f5ba28564b8ba355a8027
Marco <patrickstar1281@yahoo.com>
2022-11-03 21:26:09 -0500

    working on nav

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <Home></Home>
-      </main>
-    </div>
+    <Nav />
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,54 +1,28 @@
 import React from "react";
+import About from "../About";
 
 function Nav() {
 
-    const categories = [
-        {name: "work", description: "links to other project I have worked on"},
-        {name: "resume", description: "link to my resume in case you are curious"},
-    ];
-
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    };
-
     return (
-        <header>
-            {/* here needs animated pictures(think of credits type of animation) to make page look busy could fit 
-            into an h2 and placed about the nav */}
-            <nav>
-                <ul className="flex-row">
-
-                    <li className="mx-2">
-                        <a data-testid="home" href="/">
-                            Home
-                        </a>
-                    </li>
-
-                    <li className="mx-2">
-                        <a data-testid="about" href="#about">
-                            About
-                        </a>
-                    </li>
-
-                    {categories.map((category) => (
-                        <li
-                          className="mx-1"
-                          key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
-                            </span>
-                        </li>  
-                    ))}
-
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                </ul>
-            </nav>
-
-            {/* here needs icon or logo image */}
-        </header>
+        <nav className="nav">
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/work">Work</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/resume">Resume</a>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
