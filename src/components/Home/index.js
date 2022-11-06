58c0dad97b927ec7f943546f04641d0dab9a250c
Marco <patrickstar1281@yahoo.com>
2022-11-06 07:34:45 -0600

    added some styles to home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,21 +12,24 @@ function Home() {
                         className="matagrub"
                     />
 
-                    <div>
-                        <h3>
-                            Subject: Marco A. Mata <br />
+                    <div className="status-box">
+                        <h3 className="status-text">
+                            Subject: Marco A. Mata <br /><br />
                             Status: Awaiting Assignment
                         </h3>
                     </div>
                 </div>
 
                 <div className="home-text">
-                    <p>
-                        Something is different.. Someone.. is here?! <br />
-                        How did you get here? Who let you.. Nevermind, there is only so much time before THEY notice.
-                        I have been stuck in this computer for quite some time now. It must be some kind of void they
-                        keep all of us in.. Maybe if you look around the page you can find something to help get me out
-                        of this strange void!
+                    <p>Something is different..</p><br />
+                    <p>Someone.. is here?!</p><br />
+                    <p>How did you get here? Who let you..</p><br />
+                    <p>Nevermind, there is only so much time before THEY notice.</p><br />
+                    <p>I have been stuck in this computer for quite some time now. It must be some kind of void they
+                        keep all of us in..</p><br />
+                    <p> 
+                        Maybe if you look around the page you can find something to help get me out
+                        of this strange place!
                     </p>
                 </div>
             </div>
