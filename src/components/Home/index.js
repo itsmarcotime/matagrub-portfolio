25f5cc50f5bd05b1af393785e08489d3f1e043ac
Marco <patrickstar1281@yahoo.com>
2022-11-08 03:51:46 -0600

    added a delay to text on home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import matagrub from "../../assets/images/matagrub.jpg";
 
 function Home() {
+    const [texts, setTexts] = useState([]);
+
+    useEffect(() => {
+        const timeoutIds = [];
+
+        [
+            "...Loading...", "Something is different..", "Someone.. is here?!", "How did you get here? Who let you..",
+            "Nevermind, there is only so much time before THEY notice.",
+            "I have been stuck in this computer for quite some time now. It must be some kind of void they keep all of us in..",
+            "Maybe if you look around the page you can find something to help get me out of this strange place!",
+            "...Signal Lost.."
+        ].forEach((text, i) => {
+            const timeoutId = setTimeout(() => {
+                setTexts((prev) => [...prev, text]);
+            }, 4500 * i);
+            timeoutIds.push(timeoutId);
+        });
+        return () => {
+            timeoutIds.forEach((id) => clearTimeout(id));
+        };
+    }, []);
     return (
         <section>
             <div className="home-div">
@@ -20,7 +41,13 @@ function Home() {
                     </div>
                 </div>
 
-                <div className="home-text">
+                <div>
+                    {texts.map((text) => (
+                        <p key={text}>{text}</p>
+                    ))}
+                </div>
+
+                {/* <div className="home-text">
                     <p>Something is different..</p><br />
                     <p>Someone.. is here?!</p><br />
                     <p>How did you get here? Who let you..</p><br />
@@ -31,7 +58,7 @@ function Home() {
                         Maybe if you look around the page you can find something to help get me out
                         of this strange place!
                     </p>
-                </div>
+                </div> */}
             </div>
         </section>
     );
