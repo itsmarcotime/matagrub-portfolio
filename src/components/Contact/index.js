b119f40763a3703bcfe93a2edf2009ca4588cf14
Marco <patrickstar1281@yahoo.com>
2022-11-28 00:32:49 -0600

    started setup on the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,29 @@ import React from "react";
 function Contact () {
     return (
         <section>
-            <h1 id="about">Who am I?</h1>
+            <div className="container">
+                <div className="col-12">
+                    <h1>Contact Me</h1>
+                </div>
+                <div>
+                    <div className="container row">
+                        <div className="col-5">
+                            <ul>
+                                <li>icon</li>
+                                <li>icon</li>
+                                <li>icon</li>
+                            </ul>
+                        </div>
+                        <div className="col-5">
+                            <ul>
+                                <li>icon</li>
+                                <li>icon</li>
+                                <li>icon</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
