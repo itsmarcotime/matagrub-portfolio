de3936aeb773797fbd86aa4c206dc70e25b754a8
Marco <patrickstar1281@yahoo.com>
2022-11-28 10:55:00 -0600

    added some packages for the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,31 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 function Contact () {
     return (
         <section>
-            <div className="container">
+            <div className="container text-center">
                 <div className="col-12">
                     <h1>Contact Me</h1>
+                    <h4>You'll be glad you did.</h4>
                 </div>
                 <div>
                     <div className="container row">
                         <div className="col-5">
                             <ul>
-                                <li>icon</li>
-                                <li>icon</li>
-                                <li>icon</li>
+                                <li><FontAwesomeIcon icon={faEnvelope} className=""/></li>
+                                <li><FontAwesomeIcon icon={faEnvelope} className=""/></li>
+                                <li><FontAwesomeIcon icon={faEnvelope} className=""/></li>
                             </ul>
                         </div>
                         <div className="col-5">
                             <ul>
-                                <li>icon</li>
-                                <li>icon</li>
-                                <li>icon</li>
+                                <li><FontAwesomeIcon icon={faGithub} className=""/></li>
+                                <li><FontAwesomeIcon icon={faLinkedin} className=""/></li>
+                                <li><FontAwesomeIcon icon={faEnvelope} className=""/></li>
                             </ul>
                         </div>
                     </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faCopyright } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
 
