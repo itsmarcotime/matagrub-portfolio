cf03176dcc2522e736e36764d16994ace470294d
Marco <patrickstar1281@yahoo.com>
2022-12-02 12:45:46 -0600

    commiting work to be safe working on eyes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
-import EyeBunch from "../EyeBunch";
+import Canvas from "../Canvas";
 
 function Home() {
     const [texts, setTexts] = useState([]);
@@ -12,7 +12,7 @@ function Home() {
             "...Loading...", "Something is different... 🤔", "Someone.. is here?! 🤯", "How did you get here?? Who let you...",
             "Nevermind, there is only so much time before THEY notice. 🕒",
             "I have been stuck in this computer for quite some time now. It must be some kind of void they keep all of us in... 😵",
-            "Maybe if you look around the page you can find something to help get me out of this strange place! 🕵🕵🕵",
+            "Maybe if you look around the page you can find something to help get me out of this strange place! 🕵",
             "...Signal Lost..."
         ].forEach((text, i) => {
             const timeoutId = setTimeout(() => {
@@ -25,6 +25,14 @@ function Home() {
         };
     }, []);
 
+    const draw = (ctx, frameCount) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+        ctx.fill()
+    };
+
     return (
         <section>
             <Marquee className="marquee" gradientColor="[0,0,0]" gradientWidth="600">
@@ -35,17 +43,10 @@ function Home() {
                 You'll be glad you came. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;👾&nbsp;&nbsp;&nbsp;👾&nbsp;&nbsp;&nbsp;👾&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </Marquee>
-            <div className="m-5 row justify-content-around">
-                <div className="grubContainer col">
-                    {/* <img
-                        src={matagrub}
-                        alt="matagrub-img"
-                        className="matagrub"
-                        width={300}
-                        height={300}
-                    /> */}
-                    <EyeBunch></EyeBunch>
-                </div>
+
+            <Canvas draw={draw}></Canvas>
+
+            <div className="m-5 row justify-content-around text-center">
                 <div className="home-div col">
                     <div className="home-text">
                         {texts.map((text) => (
